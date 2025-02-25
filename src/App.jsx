b51bd8466cec6ab6176e9dacb613c55067b0f337
Abdelrahman Esmail <elawadyswe@gmail.com>
2025-02-25 22:17:02 +0200

    add authentication pages and layout component

--- src/App.jsx
+++ src/App.jsx
@@ -8,20 +8,29 @@ import Quiz from "./pages/Quiz";
 import Tips from "./pages/Tips";
 import QuestionBank from "./pages/QuestionBank";
 import RateCV from "./pages/RateCV";
+import Layout from "./pages/hocs/Layout";
+import Activate from "./pages/auth/Activate";
+import ResetPassword from "./pages/auth/ResetPassword";
+import ResetPasswordConfirm from "./pages/auth/ResetPasswordConfirm";
 
 function App() {
   return (
     <BrowserRouter>
+      <Layout>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="login" element={<LogIn />} />
         <Route path="signup" element={<SignUp />} />
+        <Route path="activate/:uid/:token" element={<Activate />} />
+        <Route path="reset-password" element={<ResetPassword />} />
+        <Route path="password/reset/confirm/:uid/:token" element={<ResetPasswordConfirm />} />
         <Route path="quiz" element={<Quiz />} />
         <Route path="tips" element={<Tips />} />
         <Route path="questionbank" element={<QuestionBank />} />
         <Route path="rateyourcv" element={<RateCV />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
+      </Layout>
     </BrowserRouter>
   );
 }

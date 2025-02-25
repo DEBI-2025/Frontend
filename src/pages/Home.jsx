a20723f49deb0f60fc5ed779949a3e760ca0afbc
Abdelrahman Esmail <elawadyswe@gmail.com>
2025-02-25 02:06:34 +0200

    fix: Correct sign-up page showing sign-in form - Fixed an issue where the sign-up page incorrectly displayed the sign-in form - Updated the component rendering logic to ensure correct form display - Design Enhancements

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,12 @@
 import styled from "styled-components";
 import Hero from "../components/Hero";
 import NavBar from "../components/NavBar";
-import FeatureCard from "../components/FeatureCard";
 
 function Home() {
   return (
     <Wrapper>
       <NavBar />
       <Hero />
-      <FeatureCard />
     </Wrapper>
   );
 }

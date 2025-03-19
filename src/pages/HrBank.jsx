15ab8137015a8f15098817d09f772dee37e3c126
Sama Ayman <samaayman46@yahoo.com>
2025-03-19 22:26:04 +0200

    feat: Complete Technical Bank and start HR Bank implementation

--- src/pages/HrBank.jsx
+++ src/pages/HrBank.jsx
@@ -1,5 +1,40 @@
+import styled from "styled-components";
+import HrOrBehavioral from "../components/HrOrBehavioral";
+
 function HrBank() {
-  return <div>HrBank</div>;
+  return (
+    <Wrapper>
+      <HrOrBehavioral />
+      <hr
+        style={{ width: "90%", borderColor: "#6A0DAD4D", borderWidth: "1.5px" }}
+      />
+    </Wrapper>
+  );
 }
 
 export default HrBank;
+const Wrapper = styled.div`
+  overflow-y: auto;
+  max-height: 89vh;
+  margin-right: 0.2rem;
+
+  &::-webkit-scrollbar {
+    width: 5px;
+  }
+
+  &::-webkit-scrollbar-track {
+    background: #8888887d;
+    border-radius: 5px;
+    margin-bottom: 0.3rem;
+  }
+
+  &::-webkit-scrollbar-thumb {
+    background: linear-gradient(to bottom, #6a0dad, #af73cf, #f7c5cc);
+    border-radius: 10px;
+  }
+
+  &::-webkit-scrollbar-thumb:hover {
+    cursor: pointer;
+    background: linear-gradient(to bottom, #4a077a, #805599, #c0949a);
+  }
+`;
